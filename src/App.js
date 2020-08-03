8409d5b36200b29fb3005bd9870bdcc3ba801071
Nadiya <nadiya.abdi@zoopla.co.uk>
2020-08-03 12:30:17 +0100

    Part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { ColorSwab } from './colorSwab';
 
 export const App = () => {
     return (
         <main>
             <h1>Color Picker</h1>
+            <label for="points">Hue:</label>
+            <input type="range" id="points" name="points" min="0" max="10"></input>
+
+            <label for="points">Saturation:</label>
+            <input type="range" id="points" name="points" min="0" max="10"></input>
+
+            <label for="points">Brightness:</label>
+            <input type="range" id="points" name="points" min="0" max="10"></input>
+
+            <ColorSwab/>
         </main>
     );
 };
---
+++ src/colorSwab.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+export const ColorSwab = () => {
+    return (
+        <div style={{background: "hsl(0, 100%, 50%)"}}/>
+    )
+}
+
